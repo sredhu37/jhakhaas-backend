e9a3cbc9a120b15c44bf23697fc2e909b34f2a76
Redhu <sunny.redhu@sap.com>
2020-05-25 19:44:17 +0530

    wip

--- utils/logger.js
+++ utils/logger.js
@@ -4,9 +4,9 @@ const path = require('path');
 const logFile = path.join(__dirname, '../logs/app.log');
 
 log4js.configure({
-  // 1 MB max file size
+  // 10 MB max file size
   appenders: {
-    file: { type: 'file', filename: logFile, maxLogSize: 1000 },
+    file: { type: 'file', filename: logFile, maxLogSize: 1000000 },
     console: { type: 'console' },
   },
   categories: { default: { appenders: ['file', 'console'], level: 'debug' } },
