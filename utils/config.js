29b46fd95e06d4a5bf1c43dec482ce2f337d42e6
i342082 <sunny.redhu@sap.com>
2020-08-05 20:50:12 +0530

    wip

--- utils/config.js
+++ utils/config.js
@@ -67,7 +67,7 @@ if (NODE_ENV === 'DEV') {
 
   const otherObj = {
     HOST: process.env.HOST_PROD || 'http://127.0.0.1',
-    PORT: process.env.PORT_PROD || 3000,
+    PORT: process.env.PORT_PROD || process.env.PORT || 3000,
     CLIENT_URL: process.env.CLIENT_URL_PROD,
   };
 
